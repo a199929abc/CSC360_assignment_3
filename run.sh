5c96c3d6e29617248a5c1bd1c3d23becff6acac2
kaiheng <a199929abc123@gmail.com>
2020-11-28 17:27:34 -0800

    commit_1.1

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 make
-# ./diskinfo ../disk3.IMA
-# ./disklist ../disk3.IMA
-# ./diskget ../disk3.IMA ANS1.PDF
-# ./diskget ../disk3.IMA REMINDER.TXT
-# ./diskput ../disk3.IMA /C.txt
+ ./diskinfo ../testNew.IMA
+ ./disklist ../disk3.IMA
+ ./diskget ../disk3.IMA ANS1.PDF
+ ./diskget ../disk3.IMA REMINDER.TXT
+ ./diskput ../disk3.IMA /C.txt
 
